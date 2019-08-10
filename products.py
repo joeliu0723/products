1a22c6e585638a671d78d535149a6bfa5d0cbde8
John Doe <joeliu0723@gmail.com>
2019-08-10 18:16:39 +0800

    print single product

--- products.py
+++ products.py
@@ -1,9 +1,12 @@
 products = []
 while True:
-	name = input('請輸入品名')
+	name = input('請輸入品名: ')
 	if name == 'q':
 		break
-	price = input('請輸入價格')
+	price = input('請輸入價格: ')
 	products.append([name, price])
 
 print(products)
+
+for p in products:
+	print(p[0], '的價格是: ',p[1])
